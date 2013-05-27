bc22a4a619e23cc8ae2da9929ab9ade7549a90d4
jacoblwe20 <jacoblowe2.0@gmail.com>
2013-05-26 18:46:16 -0700

    bumping version and building files

--- build/marrow.min.js
+++ build/marrow.min.js
@@ -1,5 +1,5 @@
 /*
- * Marrow.js - 0.0.9 
+ * Marrow.js - 0.0.13 
  * Description : Marrow is constructor that extends your constructors to help emit events and create a conventions to help manage components 
  * Project Url : https://github.com/jacoblwe20/marrow 
  * Author : Jacob Lowe <http://jacoblowe.me> 
