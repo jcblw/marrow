57dedb12a4ee0dcaf6a00551f394e631dc7eedd8
jacoblwe20 <jacoblowe2.0@gmail.com>
2013-05-26 18:26:57 -0700

    adding comments to build.js

--- src/build.js
+++ src/build.js
@@ -1,32 +1,47 @@
 (function(Marrow){
-	
-	// create an method that triggers an event
+
+	// Marrow::_extend creates a method in the prototype of the
+	// object, the first parameter is type (String), which defines the name
+	// of the method and allows binding to events on the method.
+	// Second argument is state which is the state that gets set 
+	// when the method is called, this needs to be a number. The 
+	// third argument is the stored value that allows a kinda mapping
+	// to the function that is going to be called, this also is a String.	
 	// eg. ::to("die", function(){ele.remove()})
 	// now you can bind to ::on("die")
 
 	Marrow.prototype.__extend = function(type, state, store){
 		var self = this;
-		this[type] = function(){
-			if(typeof this[store] === "function"){
-				self[store].apply(this, arguments);
+		this[ type ] = function(){
+			if( typeof this[ store ] === "function" ){
+				self[ store ].apply( this, arguments );
 			}
 
-			if(typeof state === "number"){
+			if( typeof state === "number" ){
 				self.__state = state;
 			}
 
-			self.emit(type);
+			self.emit( type );
 		};
 	};
 
-	Marrow.prototype.to = function(type, fn, state){
+	// Marrow::to creates a method that will auto fire off an event 
+	// with the same name.  The first parameter is type 
+	// which is the name of the method and the name of the event
+	// to bind to, this is a String. The second argument is a function
+	// that you would want to excute when the newly created method is
+	// called. The third state parameter is state which is a Number...
+	// the number of the state you want you component to go into once the
+	// method is called 
+
+	Marrow.prototype.to = function( type, fn, state ){
 		if(
 			typeof type === "string" &&
 			typeof fn === "function"
 		){
 			var store = "__" + type; // a `private` variable name
-			this[store] = fn;
-			this.__extend(type, state, store);
+			this[ store ] = fn;
+			this.__extend( type, state, store );
 		}
 
 	};
